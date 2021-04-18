721fa64ae1c7a4e857a9e579de3d4f4b65887bb4
Son Tran Lam <sontl@code.edu.vn>
2021-04-19 00:16:18 +0800

    temporary set empty content for cron.py

--- cron.py
+++ cron.py
@@ -1,8 +1,8 @@
-from crontab import CronTab
+# from crontab import CronTab
 
-cron = CronTab(user=True)
-job = cron.new(command="/usr/local/bin/python3 /Users/admin/Workspace/DomainTools/main.py")
-job.minute.every(1)
-cron.write()
-print(job.is_enabled())
-print(job.is_valid())
+# cron = CronTab(user=True)
+# job = cron.new(command="/usr/local/bin/python3 /Users/admin/Workspace/DomainTools/main.py")
+# job.minute.every(1)
+# cron.write()
+# print(job.is_enabled())
+# print(job.is_valid())
